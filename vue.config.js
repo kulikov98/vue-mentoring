c9d53b36fa4e991c948c9dcf311e158e7b1b146f
Vladimir Kulikov <vladimir_kulikov@epam.com>
2022-11-30 21:26:30 +0300

    add 'image-webpack-loader' for image compression

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  chainWebpack: (config) => {
+    config.module.rule('optimize-images')
+      .test(/\.(gif|png|jpe?g|svg)$/i)
+      .use('file-loader')
+        .loader('image-webpack-loader')
+          .options({
+            disable: true,
+          })
+  },
 });
