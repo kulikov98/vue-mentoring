6fdd831c14b441455ec3161515a3295d253822ca
Vladimir Kulikov <vladimir_kulikov@epam.com>
2022-12-11 20:48:03 +0300

    create component MovieDescription

---
+++ src/components/MovieParam.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+    unit?: string;
+    value?: string;
+}>();
+</script>
+
+<template>
+    <div v-if="unit && value">
+        <span class="value">{{ value }}</span>
+        <span class="unit">{{ unit }}</span>
+    </div>
+</template>
+
+<style scoped>
+    .value {
+        margin-right: 5px;
+        color: var(--pink);
+        font-size: 28px;
+        font-weight: 300;
+    }
+    .unit {
+        color: var(--gray-text-dark);
+        font-size: var(--font-size-medium);
+    }
+</style>
