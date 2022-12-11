4b6e1b98a7f0719d0863701bf366ba25482603c6
Vladimir Kulikov <vladimir_kulikov@epam.com>
2022-12-11 20:43:35 +0300

    create component AppButtonToggle

---
+++ src/components/AppButtonToggle.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+    buttons: Array<{ name: string }>,
+    selected?: string,
+    title?: string,
+}>();
+
+const selected = ref(props.selected
+  ? props.selected
+  : props.buttons[0].name);
+
+const emit = defineEmits(['selected']);
+</script>
+
+<template>
+    <div class="button-toggle">
+        <span class="title" v-if="title">{{ title }}</span>
+        <label v-for="btn in buttons" :key="btn.name" :class="{ selected: selected === btn.name }">
+            {{ btn.name }}
+            <input
+                name="btn-toggle"
+                type="radio"
+                @change="emit('selected', btn.name)"
+                v-model="selected" :value="btn.name"
+            >
+        </label>
+    </div>
+</template>
+
+<style scoped>
+.button-toggle {
+    display: flex;
+    align-items: center;
+}
+.title {
+    margin-right: 15px;
+    color: var(--white);
+}
+
+input {
+    display: none;
+}
+
+label {
+    padding: 8px 20px;
+    font-size: var(--font-size-small);
+    background-color: var(--gray-light);
+    color: var(--white);
+    cursor: pointer;
+}
+
+label:first-of-type {
+    border-top-left-radius: var(--form-radius);
+    border-bottom-left-radius: var(--form-radius);
+}
+
+label:last-of-type {
+    border-top-right-radius: var(--form-radius);
+    border-bottom-right-radius: var(--form-radius);
+}
+
+.selected {
+    background-color: var(--pink);
+}
+</style>
---
+++ src/stories/AppButtonToggle.stories.js
@@ -0,0 +1,34 @@
+import AppButtonToggle from '../components/AppButtonToggle.vue';
+
+export default {
+  name: 'AppButtonToggle',
+  component: AppButtonToggle,
+  argTypes: {
+    onChange: {},
+  },
+};
+
+const Template = (args) => ({
+  components: { AppButtonToggle },
+  setup() {
+    return { args };
+  },
+  template: '<AppButtonToggle :="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  buttons: [{ name: 'ONE' }, { name: 'TWO' }, { name: 'TREE' }],
+};
+
+export const WithSelected = Template.bind({});
+WithSelected.args = {
+  ...Default.args,
+  selected: Default.args.buttons[1].name,
+};
+
+export const WithTitle = Template.bind({});
+WithTitle.args = {
+  ...Default.args,
+  title: 'SEARCH BY',
+};
