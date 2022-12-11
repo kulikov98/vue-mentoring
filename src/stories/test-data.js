8aea182c82bf9d5a0763d023185fff90b6ef240f
Vladimir Kulikov <vladimir_kulikov@epam.com>
2022-12-11 20:48:42 +0300

    add test data for storybook

---
+++ src/stories/test-data.js
@@ -0,0 +1,34 @@
+import posterPulpiction from './assets/poster-pulp-fiction.jpeg';
+import posterFightClub from './assets/poster-fight-club.jpeg';
+import posterForrestGump from './assets/poster-forrest-gump.jpeg';
+
+// eslint-disable-next-line
+export const movies = [
+  {
+    name: 'Pulp Fiction',
+    genre: 'Oscar winning movie',
+    image: posterPulpiction,
+    year: '1994',
+    duration: '154',
+    description: 'A burger-loving hit man, his philosophical partner, a drug-addled gangster\'s moll and a washed-up boxer converge in this sprawling, comedic crime caper. Their adventures unfurl in three stories that ingeniously trip back and forth in time.',
+    rating: '8.5',
+  },
+  {
+    name: 'Fight Club',
+    genre: 'Drama, Thriller, Comedy',
+    image: posterFightClub,
+    year: '1999',
+    duration: '124',
+    description: 'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
+    rating: '8.4',
+  },
+  {
+    name: 'Forrest Gump',
+    genre: 'Comedy, Drama, Romance',
+    image: posterForrestGump,
+    year: '1994',
+    duration: '119',
+    description: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case, far exceeding what anyone imagined he could do. But despite all he has achieved, his one true love eludes him.',
+    rating: '8.5',
+  },
+];
