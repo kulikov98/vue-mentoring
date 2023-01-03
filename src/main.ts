05ef06d971b69c3b531880df6cfb29f79363410a
Vladimir Kulikov <vladimir_kulikov@epam.com>
2023-01-03 23:35:04 +0300

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import './styles.css';
 import image from './directives/image';
@@ -9,6 +10,7 @@ import image from './directives/image';
 library.add(faMagnifyingGlass);
 
 createApp(App)
+  .use(createPinia())
   .component('font-awesome-icon', FontAwesomeIcon)
   .directive('image', image)
   .mount('#app');
