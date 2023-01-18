81465acb662d0c46884676a229d4deb64b513710
Vladimir Kulikov <vladimir_kulikov@epam.com>
2023-01-18 11:54:38 +0300

    add simplified suggestions

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-/* eslint-disable vuejs-accessibility/form-control-has-label */
+/* eslint-disable vuejs-accessibility/label-has-for */
 import { ref, watch } from 'vue';
 
 interface Props {
   initValue?: string;
   placeholder?: string;
+  suggestions?: string[];
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -19,16 +20,37 @@ watch(text, () => emit('input', text.value));
 </script>
 
 <template>
-  <input
-    class="input"
-    type="text"
-    v-model="text"
-    :placeholder="placeholder"
-    @keydown.enter="emit('keydown-enter')"
-  />
+  <label>
+    <input
+      class="input"
+      type="text"
+      v-model="text"
+      :placeholder="placeholder"
+      @keydown.enter="emit('keydown-enter')"
+    />
+    <ul
+      class="suggestions"
+      v-if="suggestions?.length"
+    >
+      <li
+        class="suggestion"
+        v-for="suggestion in suggestions"
+        :key="suggestion"
+        @click="text = suggestion"
+        @keydown.enter="text = suggestion"
+      >
+        {{ suggestion }}
+      </li>
+    </ul>
+  </label>
+  <!-- <span style="color: white">{{ suggestions }}</span> -->
 </template>
 
 <style scoped>
+label {
+  position: relative;
+  width: 100%;
+}
 .input {
   width: 100%;
   height: 55px;
@@ -39,10 +61,30 @@ watch(text, () => emit('input', text.value));
   outline: none;
   color: var(--white);
   background-color: var(--transparent-black);
+  box-sizing: border-box;
 }
 
 .input::placeholder {
   color: var(--gray-light);
   font-size: var(--font-size-large);
+}
+
+.suggestions {
+  position: absolute;
+  background: white;
+  top: 55px;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  list-style: none;
+}
+
+.suggestion {
+  padding: 10px 20px;
+}
+
+.suggestion:hover {
+  cursor: pointer;
+  background-color: grey;
 }
 </style>
